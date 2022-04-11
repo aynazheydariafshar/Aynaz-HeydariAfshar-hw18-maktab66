41a4287ef6fac9867a9b55147c57defa1d03c86a
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-04-11 17:01:30 +0430

    add project and and button for change

---
+++ ex1/src/App.js
@@ -0,0 +1,27 @@
+import {useState} from "react";
+import './Assests/Styles/App.css';
+import ButtonChangePage from './Components/ButtonChangePage';
+import Login from './Components/Login';
+import Register from './Components/Register';
+
+function App() {
+  const [status , statestatus] = useState('login');
+
+  const radioHandler = (input) => {
+    statestatus(input)
+  }
+  return (
+    <div className="App">
+     <ButtonChangePage 
+      checkitregister = {status === 'register'}
+      checkitlogin = {status === 'login'}
+      clicklogin = {(e) => radioHandler(e.target.value)}
+      clickregister = {(e) => radioHandler(e.target.value)}
+      />
+      {status === 'login' && <Login />}
+      {status === 'register' && <Register />}
+    </div>
+  );
+}
+
+export default App;
